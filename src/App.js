915c51774ba5fe2c6da0e2c041378d4244df814b
Jonathan Lee <jonathan.1994.lee@gmail.com>
2021-08-27 15:15:43 -0700

    Include changes to CSS and List.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,16 @@ import data from './data';
 import List from './components/List';
 
 const App = () => {
+	const [people, setPeople] = useState(data);
+
 	return (
-		<div>
-			<h2>reminder project setup</h2>
-		</div>
+		<main>
+			<section className='container'>
+				<h3>{people.length} birthdays today</h3>
+				<List people={people} />
+				<button onClick={() => setPeople([])}>clear all</button>
+			</section>
+		</main>
 	);
 };
 
